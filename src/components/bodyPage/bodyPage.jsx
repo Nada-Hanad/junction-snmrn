9f87405688ab2f17b140c046eb356e53b5126415
RaniaRez <raniarezkellah2002@gmail.com>
2021-12-17 18:52:37 +0100

    6th

--- src/components/bodyPage/bodyPage.jsx
+++ src/components/bodyPage/bodyPage.jsx
@@ -3,11 +3,15 @@ import './bodyPage.css'
 export default function BodyPage(){
     return(
         <div className="body-page">
+            <div className="big-container"></div>
+            <div className="container">
             <div className="btn1" >
-               <h1>Diagnosis</h1>
+               <h1 style={{fontFamily:'Poppins'}}>Diagnosis</h1>
             </div>
             <div className="btn2" >
-              <h1>Information</h1>
+              <h1 style={{fontFamily:'Poppins'}}>Information</h1>
+            </div>
+
             </div>
         </div>
 
